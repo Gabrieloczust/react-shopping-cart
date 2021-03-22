34982f6a29eedc1b9c7afe8b2e7e3f75ce8bd5e6
Gabriel Fagundes Oczust <gabriel.oczust@meta.com.br>
2021-03-22 11:28:45 -0300

    # Home

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -1,7 +1,7 @@
-import { createContext, ReactNode, useContext, useState } from 'react';
-import { toast } from 'react-toastify';
-import { api } from '../services/api';
-import { Product, Stock } from '../types';
+import { createContext, ReactNode, useContext, useState } from "react";
+import { toast } from "react-toastify";
+import { api } from "../services/api";
+import { Product, Stock } from "../types";
 
 interface CartProviderProps {
   children: ReactNode;
@@ -23,7 +23,7 @@ const CartContext = createContext<CartContextData>({} as CartContextData);
 
 export function CartProvider({ children }: CartProviderProps): JSX.Element {
   const [cart, setCart] = useState<Product[]>(() => {
-    const storagedCart = localStorage.getItem('@RocketShoes:cart');
+    const storagedCart = localStorage.getItem("@RocketShoes:cart");
 
     if (storagedCart) {
       return JSON.parse(storagedCart);
@@ -32,9 +32,35 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
     return [];
   });
 
+  const updateCart = (cartArray: Product[]) => {
+    setCart(cartArray);
+    localStorage.setItem("@RocketShoes:cart", JSON.stringify(cartArray));
+    console.log(cart);
+  };
+
   const addProduct = async (productId: number) => {
     try {
-      // TODO
+      const existProduct = cart.some((product) => product.id === productId);
+
+      if (existProduct) {
+        const cartAddAmount = cart.map((product) => {
+          if (product.id === productId) {
+            product.amount += 1;
+            return product;
+          }
+
+          return product;
+        });
+
+        updateCart(cartAddAmount);
+      } else {
+        const productResponse = await api.get(`products/${productId}`);
+
+        if (productResponse.status === 200) {
+          productResponse.data.amount = 1;
+          updateCart([...cart, productResponse.data]);
+        }
+      }
     } catch {
       // TODO
     }
